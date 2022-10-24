d4f6af58daa0c5129fed3c60c3cacf22d05f0d5c
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-10-24 16:42:12 -0400

    add index route integration test

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -14,7 +14,8 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['tests','nightwatch/examples'],
+  // src_folders: ['tests','nightwatch/examples'], (default)
+  src_folders: ['tests/integration'],
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
   page_objects_path: ['nightwatch/page-objects'],
