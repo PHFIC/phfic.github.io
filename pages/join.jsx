b5a57e49841741310e221c30fd8b11a6bee58d87
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-10-28 09:52:23 -0400

    create join page

---
+++ pages/join.jsx
@@ -0,0 +1,16 @@
+import Title from '../components/title'
+import Section from '../components/section'
+import Alert from 'react-bootstrap/Alert'
+import WorkInProgress from '../components/work_in_progress'
+
+export default function Calendar() {
+  return (
+    <Section id="main">
+      <Title prefix="Join" />
+
+      <h1 className="text-center w-100 my-3">Join the PHFIC Community</h1>
+      
+      <WorkInProgress name="Join PHFIC - WIP" />
+    </Section>
+  )
+}
