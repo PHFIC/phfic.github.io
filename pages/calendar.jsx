c3fed532488ed47f1f81dcb90289c368bfd6c1aa
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-12-01 16:43:47 -0500

    sketch out calendar#main

--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -1,17 +1,39 @@
 import Title from '../components/title'
 import Section from '../components/section'
-import Alert from 'react-bootstrap/Alert'
 import WorkInProgress from '../components/work_in_progress'
+import { Row, Col, ListGroup } from 'react-bootstrap'
 
 export default function Calendar() {
   return (
-    <div style={{minHeight: "90vh"}}>
-      <Section id="main" debug="true">
-        <Title prefix="Calendar" />
+    <div>
+      <Title prefix="Calendar" />
 
-        <h1 className="text-center w-100 my-3">PHFIC Calendar</h1>
+      <WorkInProgress name="Calendar Page WIP" className="m-5" />
 
-        <WorkInProgress name="Calendar Page WIP" />
+      <Section id="main">
+        <Row>
+          <Col xs={12}>
+            <h1 className="text-center w-100 my-3">PHFIC Calendar</h1>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={6} className="text-center">
+            <h2 className="">Next: Office Hours</h2>
+            <h3 className="">{/* TODO */} &lt;Calendar Day Icon&gt;</h3>
+            <p>Link/Location goes here</p>
+            <p>And countdown timer...</p>
+          </Col>
+          <Col sm={6} className="px-4 px-sm-5">
+            <ListGroup>
+              <ListGroup.Item>Office Hours <span className="text-end">Wednesday, Jan Xth 2022</span></ListGroup.Item>
+              <ListGroup.Item>Office Hours <span className="text-end">Wednesday, Jan Yth 2022</span></ListGroup.Item>
+              <ListGroup.Item>Steering Committee Meeting <span className="text-end">Tuesday, Jan Zth 2022</span></ListGroup.Item>
+              <ListGroup.Item>FHIR CDC Community of Practice <span className="text-end">Monday, Jan Wth 2022</span></ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+
       </Section>
 
       <Section id="office-hours" debug="true"></Section>
