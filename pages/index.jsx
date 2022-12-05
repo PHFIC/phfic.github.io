ee4eaa6a417819ac459eeaac0168ff6c44d5aecf
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-12-05 18:40:46 -0500

    update colors

--- pages/index.jsx
+++ pages/index.jsx
@@ -61,7 +61,7 @@ export default function Home() {
 
       </Section>
 
-      <Section id="about" className="bg-success" noMinHeight={true}>
+      <Section id="about" className="bg-secondary" noMinHeight={true}>
           <h1 className="display-3 w-100 text-center">Our objectives are to</h1>
           <Row md={1} lg={3} className="justify-content-around g-4 w-100">
             <GridCard src={CommunityImg} alt="Build Community" title="Build a Community">
@@ -83,7 +83,7 @@ export default function Home() {
             </GridCard>
           </Row>
           <Row className="justify-content-center justify-content-md-end">
-            <Button variant="primary" size="lg" className="my-3 me-md-5" style={{width: "20rem"}} href="https://www.healthit.gov/topic/standards-technology/standards/fhir-fact-sheets" target="_blank">
+            <Button variant="dark" size="lg" className="my-3 me-md-5" style={{width: "20rem"}} href="https://www.healthit.gov/topic/standards-technology/standards/fhir-fact-sheets" target="_blank">
                 What is FHIR&reg;? &#x02197;
             </Button>
           </Row>
@@ -107,22 +107,22 @@ export default function Home() {
           </Row>
       </Section>
 
-      <Section id="collaborative-structure" className="bg-secondary position-relative" noMinHeight={true}>
+      <Section id="collaborative-structure" className="bg-light position-relative border border-dark" noMinHeight={true}>
         <Image src={StructureImg} style={{objectFit:"contain",width:"100vw"}} alt="PHFIC is composed of a Steering Commitee, Advisory Services, Public Health Players, and Pilot or Real World Activities that form a feedback loop for realizing the CDC Data Modernization Initiative."/>
       </Section>
 
-      <Section id="spotlight" noMinHeight="true" className="bg-dark mb-0">
+      <Section id="spotlight" noMinHeight="true" className="bg-secondary mb-0">
         <Row className="row-cols-2-reverse g-0">
           <Col>
             <DarkCarousel slides={spotlight}/>
           </Col>
           <Col className="my-auto">
-            <h1 className="w-100 text-center display-2 text-light">Recent Activity Spotlight</h1>
+            <h1 className="w-100 text-center display-2 text-dark">Recent Activity Spotlight</h1>
           </Col>
         </Row>
       </Section>
 
-      <Section id="pilot-site-map" className="bg-light position-relative" noMinHeight="true">
+      <Section id="pilot-site-map" className="bg-light position-relative border border-dark" noMinHeight="true">
         <h1 className="w-100 text-center display-3 position-relative" style={{zIndex: "1"}}>PHFIC Pilot Sites</h1>
         <Image src={PilotSiteMapImg}
                alt="PHFIC has a total 3 pairs of pilot sites that are Washington State-King County, Minnesota State-Hennepin County, and Virginia State-Fairfax County."
@@ -133,9 +133,9 @@ export default function Home() {
       <Section id="get-started" className="bg-primary text-center align-middle">
           <h1 className="w-100 text-center text-light display-2 pt-2 mb-3">Dive Deeper...</h1>
           <Row className="row-cols-lg-2 position-relative bottom-0">
-            <Tile href="/join" title="Join PHFIC" variant="success"/>
-            <Tile href="/playbook" title="Playbook" variant="danger"/>
-            <Tile href="/resources#office-hours" title="Office Hours" variant="warning"/>
+            <Tile href="/join" title="Join PHFIC" variant="dark"/>
+            <Tile href="/playbook" title="Playbook" variant="dark"/>
+            <Tile href="/resources#office-hours" title="Office Hours" variant="dark"/>
             <Tile href="/404" title="More Coming Soon!" variant="dark" disabled/>
           </Row>
       </Section>
