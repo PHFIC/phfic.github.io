f87556306a0ab18f019590825d5fbc31faaa9599
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-12-21 12:47:40 -0500

    improve landing buttons

--- pages/index.jsx
+++ pages/index.jsx
@@ -33,17 +33,17 @@ export default function Home() {
             <Container fluid style={{height: "min-content"}}>
               <Row className="justify-content-center my-3">
                 <Col xs={10} md={9} className="my-md-3 my-xs-5">
-                  <a href="/playbook/TODO" className="btn btn-lg btn-primary d-block">What is FHIR?</a>
+                  <a href="/playbook/TODO" className="btn btn-lg btn-primary d-block text-light">What is FHIR?</a>
                 </Col>
               </Row>
               <Row className="justify-content-center my-3">
                 <Col xs={10} md={9} className="my-md-3 my-xs-5">
-                  <a href="/playbook/TODO" className="btn btn-lg d-block" style={{backgroundColor: "var(--bs-blue)"}}>Starting FHIR-Ready Organizations</a>
+                  <a href="/playbook/TODO" className="btn btn-lg d-block text-light" style={{backgroundColor: "var(--bs-blue)"}}>Starting FHIR-Ready Organizations</a>
                 </Col>
               </Row>
               <Row className="justify-content-center my-3">
                 <Col xs={10} md={9} className="my-md-3 my-xs-5">
-                  <a href="/playbook/TODO" className="btn btn-lg d-block" style={{backgroundColor: "var(--bs-indigo)"}}>Implementing FHIR</a>
+                  <a href="/playbook/TODO" className="btn btn-lg d-block text-light" style={{backgroundColor: "var(--bs-indigo)"}}>Implementing FHIR</a>
                 </Col>
               </Row>
             </Container>
