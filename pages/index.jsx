fb7c8b76090c144b438cc091153d32c9163e2b43
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-12-23 15:02:41 -0500

    factorize logo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,15 @@
 // pages/index.jsx -> renders / (home page)
 
 import Title from '../components/title'
-import Section from '../components/section'
-import ButtonStack from '../components/button_stack'
 import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import ButtonStack from '../components/button_stack'
+import LandingLogo from '../components/landing_logo'
+
+import Section from '../components/section'
 import Image from 'next/image'
 
 // image imports
-import LogoImg from '../public/PHFIC_Secondary_COL.svg'
+import Logo from '../public/PHFIC_Secondary_COL.svg'
 
 // view logic + content
 export default function Home() {
@@ -19,16 +21,10 @@ export default function Home() {
       <Container fluid id="landing" style={{minHeight: "80vh"}}>
         <Row className="justify-content-between h-100">
           <Col md={7} className="text-center py-5">
-            <h1 className="d-none">Public Health FHIR&reg; Implementation Collaborative</h1>
-            <div className="position-relative w-100" style={{height: "40vh"}}>
-              <Image src={LogoImg}
-                     alt="PHFIC | Public Health FHIR&reg; Implementation Collaborative"
-                     priority
-                     fill
-              />
-            </div>
-            <hr className="d-none d-lg-inline-block bg-primary opacity-100 border border-0" style={{height: '10px', width: '80%'}}/>
-            <h3 className="d-none d-lg-block">Promoting Public Health Data Exchange for the Modern Age</h3>
+            <LandingLogo seo="PHFIC (Public Health FHIR® Implementation Collaborative)"
+                         img={Logo}
+                         alt="Public Health FHIR® Implementation Collaborative"
+                         tagline="Promoting Public Health Data Exchange for the Modern Age" />
           </Col>
           <Col md={4} className="d-flex align-items-center">
             <ButtonStack buttons={[
