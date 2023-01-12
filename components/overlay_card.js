ba4f663c04fee0355371feadebcaa257c7e7a6dc
Shaumik-Ashraf <shaumikashraf@mitre.org>
2023-01-12 16:26:46 -0500

    improve mobile view for overlay_card, split_pane components

--- components/overlay_card.js
+++ components/overlay_card.js
@@ -14,7 +14,7 @@ export default function OverlayCard(props) {
     <Card className="bg-dark text-white border-0 rounded-0" style={{height:"85vh"}}>
       <Image className="card-img rounded-0" src={props.img} alt={props.alt} fill={true} style={{objectFit: "cover"}} />
       <Card.ImgOverlay>
-        <Card.Title className="text-center fw-bold p-5" as="h2">{props.title}</Card.Title>
+        <Card.Title className="text-center fw-bold pt-5" as="h2">{props.title}</Card.Title>
       </Card.ImgOverlay>
     </Card>
   );
